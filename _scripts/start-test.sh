35c6a288117e489aaeb1e2d7c451dc967b9c7d7a
alfred <alfred.chen@104.com.tw>
2022-11-02 17:55:46 +0800

    feat: use pm2

--- _scripts/start-test.sh
+++ _scripts/start-test.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 BASEDIR=/opt/cicd-test
+APPNAME = cicd-test
+LOGS="${BASEDIR}/logs";
+mkdir -p $LOGS
 cd $BASEDIR
 
-npm start
+while(pm2 list |grep ${APPNAME} -c)
+do
+   pm2 delete elearning-admin
+    sleep 1
+    echo 'wake up'
+done
+
+pm2 start app.js --name ${APPNAME}
