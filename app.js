0d04136230481a6e14caa748fad7e9c0e744012b
alfred <alfred.chen@104.com.tw>
2022-11-03 13:56:59 +0800

    add health check route

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/status', (req, res) => {
+  return res.json({code:200, message:'process on'}).end();
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
