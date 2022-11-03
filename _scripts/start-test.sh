4f058abb46606647cc286fe6a3f2dc3b9f1824d3
alfred <alfred.chen@104.com.tw>
2022-11-03 11:18:23 +0800

    fix: start-test.sh

--- _scripts/start-test.sh
+++ _scripts/start-test.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/bashnano
 BASEDIR=/opt/cicd-test
-APPNAME = cicd-test
+APPNAME=cicd-test
 LOGS="${BASEDIR}/logs";
 mkdir -p $LOGS
 cd $BASEDIR
 
 while(pm2 list |grep ${APPNAME} -c)
 do
-   pm2 delete elearning-admin
+   pm2 delete ${APPNAME}
     sleep 1
     echo 'wake up'
 done
