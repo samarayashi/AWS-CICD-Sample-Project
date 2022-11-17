5a3d0c210a053875091d6e9d37b3401bd092adfb
alfred <alfred.chen@104.com.tw>
2022-11-17 09:46:30 +0800

    warp /alfred-test in domain   - express.Router

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/alfred-test/', (req, res) => {
+let router = express.Router()
+router.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
-app.get('/alfred/status', (req, res) => {
+router.get('/status', (req, res) => {
   return res.json({code:200, message:'process on'}).end();
 })
 
+app.use('/alfred-test', router)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
